348291953f91b4d9599cde98853bfa49d94a2eba
nada_ayman_azzam <n.azzam@kioncm.com>
2020-09-18 02:37:17 +0200

    deploy my app

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './style/index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {  BrowserRouter } from 'react-router-dom';
+import {  HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
   {/* <AuthProvider> */}
   <App />
   {/* </AuthProvider> */}
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
