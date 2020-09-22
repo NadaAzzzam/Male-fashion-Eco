1f6e2d484baeae7fda334d5f2cff4c0ef955a0a6
nada_ayman_azzam <n.azzam@kioncm.com>
2020-09-22 23:46:04 +0200

    change logo

---
+++ build/precache-manifest.997e11b98e833b13476e6d15baa96561.js
@@ -0,0 +1,254 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f14b19b69d3695b14252162686c0c4b4",
+    "url": "/Male-fashion-Eco/index.html"
+  },
+  {
+    "revision": "3ab7b26dd6f3a4954e54",
+    "url": "/Male-fashion-Eco/static/css/5.7c7dfd06.chunk.css"
+  },
+  {
+    "revision": "7b9d1d195d00660a5c48",
+    "url": "/Male-fashion-Eco/static/css/main.0458d0b1.chunk.css"
+  },
+  {
+    "revision": "ba3182f14df90b1e547b",
+    "url": "/Male-fashion-Eco/static/js/0.d418244e.chunk.js"
+  },
+  {
+    "revision": "c40a60c6a3b23f31793e",
+    "url": "/Male-fashion-Eco/static/js/1.908ac289.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/Male-fashion-Eco/static/js/1.908ac289.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03b7170bee46e6802fa4",
+    "url": "/Male-fashion-Eco/static/js/10.186f48e8.chunk.js"
+  },
+  {
+    "revision": "dcee3f2c55e7e538522b",
+    "url": "/Male-fashion-Eco/static/js/11.1ef0a663.chunk.js"
+  },
+  {
+    "revision": "979c6a14a920a8106852",
+    "url": "/Male-fashion-Eco/static/js/12.6728d661.chunk.js"
+  },
+  {
+    "revision": "45367195cac413caf6fd",
+    "url": "/Male-fashion-Eco/static/js/13.6dbc6059.chunk.js"
+  },
+  {
+    "revision": "35be63b0ce8f524bee81",
+    "url": "/Male-fashion-Eco/static/js/14.778ea75e.chunk.js"
+  },
+  {
+    "revision": "b7ca3ad930af36e62490",
+    "url": "/Male-fashion-Eco/static/js/15.3cc2216d.chunk.js"
+  },
+  {
+    "revision": "2f9e4952adec78459d24",
+    "url": "/Male-fashion-Eco/static/js/16.158121c0.chunk.js"
+  },
+  {
+    "revision": "6f04cf4e8b0af9523be4",
+    "url": "/Male-fashion-Eco/static/js/17.c58d8efb.chunk.js"
+  },
+  {
+    "revision": "224445d0066e94bf635f",
+    "url": "/Male-fashion-Eco/static/js/2.4dda4c50.chunk.js"
+  },
+  {
+    "revision": "3ab7b26dd6f3a4954e54",
+    "url": "/Male-fashion-Eco/static/js/5.72b00120.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Male-fashion-Eco/static/js/5.72b00120.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d537be61bf5c32fed129",
+    "url": "/Male-fashion-Eco/static/js/6.5c377c32.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/Male-fashion-Eco/static/js/6.5c377c32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9c9162081b735adbf8f",
+    "url": "/Male-fashion-Eco/static/js/7.38d02684.chunk.js"
+  },
+  {
+    "revision": "883591b3ca8b241d006b",
+    "url": "/Male-fashion-Eco/static/js/8.151455b2.chunk.js"
+  },
+  {
+    "revision": "ae492162b32133714608",
+    "url": "/Male-fashion-Eco/static/js/9.44b10840.chunk.js"
+  },
+  {
+    "revision": "7b9d1d195d00660a5c48",
+    "url": "/Male-fashion-Eco/static/js/main.d5a5f87e.chunk.js"
+  },
+  {
+    "revision": "9a81306ca9ec26ea16b9",
+    "url": "/Male-fashion-Eco/static/js/runtime-main.7bc7a522.js"
+  },
+  {
+    "revision": "e34ad5dad362d3ef63c5fa02d84c0a5a",
+    "url": "/Male-fashion-Eco/static/media/banner-1.e34ad5da.jpg"
+  },
+  {
+    "revision": "39a5f84a68605f292bcf4f78a8e3097e",
+    "url": "/Male-fashion-Eco/static/media/banner-2.39a5f84a.jpg"
+  },
+  {
+    "revision": "b0e0e876ca7b09f13f75295c51e84ed6",
+    "url": "/Male-fashion-Eco/static/media/banner-3.b0e0e876.jpg"
+  },
+  {
+    "revision": "8b3b67a34008f04c83640170b9efd87c",
+    "url": "/Male-fashion-Eco/static/media/details-payment.8b3b67a3.png"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/Male-fashion-Eco/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/Male-fashion-Eco/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/Male-fashion-Eco/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/Male-fashion-Eco/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/Male-fashion-Eco/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/Male-fashion-Eco/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/Male-fashion-Eco/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/Male-fashion-Eco/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/Male-fashion-Eco/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/Male-fashion-Eco/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/Male-fashion-Eco/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/Male-fashion-Eco/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/Male-fashion-Eco/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/Male-fashion-Eco/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/Male-fashion-Eco/static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "81db72f5c179c570d249528f4ef1546a",
+    "url": "/Male-fashion-Eco/static/media/hero-1.81db72f5.jpg"
+  },
+  {
+    "revision": "5372893c4f0d988a839431666ea80e0d",
+    "url": "/Male-fashion-Eco/static/media/hero-2.5372893c.jpg"
+  },
+  {
+    "revision": "b391fa008463e7158e901247c5ca7d40",
+    "url": "/Male-fashion-Eco/static/media/instagram-1.b391fa00.jpg"
+  },
+  {
+    "revision": "60b19b469fbb7cf1e99150eaff91c2d3",
+    "url": "/Male-fashion-Eco/static/media/instagram-2.60b19b46.jpg"
+  },
+  {
+    "revision": "ec4ba2ad81e286f6f5f461ea35e44b30",
+    "url": "/Male-fashion-Eco/static/media/instagram-3.ec4ba2ad.jpg"
+  },
+  {
+    "revision": "2955799dbb6133642f823622167c2071",
+    "url": "/Male-fashion-Eco/static/media/product-12.2955799d.jpg"
+  },
+  {
+    "revision": "03fe07cda2c40fe79231827810d9c2c2",
+    "url": "/Male-fashion-Eco/static/media/product-4.03fe07cd.jpg"
+  },
+  {
+    "revision": "1cee96b8b18c7bcad350e1340aaed9c6",
+    "url": "/Male-fashion-Eco/static/media/product-big-2.1cee96b8.png"
+  },
+  {
+    "revision": "51028f1e0a6bec6efedc777be4ee0746",
+    "url": "/Male-fashion-Eco/static/media/product-big-3.51028f1e.png"
+  },
+  {
+    "revision": "e638438fba93463927304c6b1cc87a6f",
+    "url": "/Male-fashion-Eco/static/media/product-big-4.e638438f.png"
+  },
+  {
+    "revision": "e1297470d7737a40c4598724dac2c89f",
+    "url": "/Male-fashion-Eco/static/media/product-big.e1297470.png"
+  },
+  {
+    "revision": "accf91f906df1c14efdc21d377eb94e9",
+    "url": "/Male-fashion-Eco/static/media/product-sale.accf91f9.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Male-fashion-Eco/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Male-fashion-Eco/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Male-fashion-Eco/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Male-fashion-Eco/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "9789e870a896e56d9a9af18b7577b3c5",
+    "url": "/Male-fashion-Eco/static/media/thumb-1.9789e870.png"
+  },
+  {
+    "revision": "204a0100f7761b614e7f62846c4ad875",
+    "url": "/Male-fashion-Eco/static/media/thumb-2.204a0100.png"
+  },
+  {
+    "revision": "f6c16c3ddce2a07e1eb0627c93f1668c",
+    "url": "/Male-fashion-Eco/static/media/thumb-3.f6c16c3d.png"
+  },
+  {
+    "revision": "0d01b2cc536e4eaa3184f0239ec010f1",
+    "url": "/Male-fashion-Eco/static/media/thumb-4.0d01b2cc.png"
+  }
+]);
